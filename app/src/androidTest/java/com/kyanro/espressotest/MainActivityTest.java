9d1f08d11d3b7d57cbe2f153098293d3a199e71f
kyanro <gw.fnct+github@gmail.com>
2015-05-13 00:26:46 +0900

    1st commit

---
+++ app/src/androidTest/java/com/kyanro/espressotest/MainActivityTest.java
@@ -0,0 +1,37 @@
+package com.kyanro.espressotest;
+
+import android.support.test.espresso.Espresso;
+import android.support.test.internal.runner.junit4.AndroidJUnit4ClassRunner;
+import android.support.test.runner.AndroidJUnit4;
+import android.test.ActivityInstrumentationTestCase2;
+import android.test.suitebuilder.annotation.LargeTest;
+
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import static android.support.test.espresso.Espresso.*;
+import static org.junit.Assert.*;
+
+/**
+ * Created by ppp on 2015/05/12.
+ */
+@RunWith(AndroidJUnit4.class)
+@LargeTest
+public class MainActivityTest {
+
+    @Rule
+    public ActivityTestRule
+
+    @Override
+    public void setUp() throws Exception {
+        super.setUp();
+        getActivity();
+    }
+
+    @Test
+    public void ‰‚ß‚Ä‚ÌespressoƒeƒXƒg() {
+        onView()
+    }
+
+}
